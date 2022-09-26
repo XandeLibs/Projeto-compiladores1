6a8a818919931b01cf0671a0142ebc071306f880
Alexandre Libório <alexandre_liborio@outlook.com>
2022-09-25 22:08:59 -0400

    Reescrita da lista e pequenas correções

--- linkedlist.c
+++ linkedlist.c
@@ -2,61 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_STR_LEN 255
+
 typedef struct link_node {
 	char node_str[MAX_STR_LEN];
 	struct link_node *next;
 } node;
 
 int compare_node(struct link_node *n1, struct link_node *n2);
-struct link_node *add_node(struct link_node	*list, struct link_node *node);
+struct link_node *compare_list(struct link_node *list, char str[]);
+void add_node(struct link_node	*list, char ID[]);
 void display_list(struct link_node *head);
 void free_list(node *head);
 void cleanInput(char* input);
 
-void add(char ID[]) {
-	node *head, *newNode;
-	head = NULL;
-	newNode = (node*)malloc(sizeof(node));
-	strcpy(newNode->node_str, input);
-	newNode->next = NULL;
-	if (input[0] != '\0') {
-		head = add_node(head, newNode);
+void add(struct link_node *list, char ID[]) {
+	node *newNode;
+	if (list == NULL){
+		newNode = (node*)malloc(sizeof(node));
+		strcpy(newNode->node_str, ID);
+		newNode->next = NULL;
+		list = newNode;
 	}
-	display_list(head);
+	else{
+		printf("newnode com lista\n");
+		newNode = compare_list(list, ID);
+		if (newNode != NULL){
+			add_node(newNode, ID);
+		}
+	}
 }
 
-struct link_node *add_node(struct link_node	*list, struct link_node *node){
-   int comp;
-	struct link_node *prev, *next;
-	if (!list) {
-		list = node;
-	}
-	else {
-		prev = NULL;
-		next = list;
-		while (next && compare_node(node,next)>-1) {
-         comp = compare_node(node,next);
-			prev = next;
-			next = next->next;
+//Apenas listas não vazias
+struct link_node *compare_list(struct link_node *list, char str[]){
+	node *node = list;
+	int comp = 0;
+	while (node->next != NULL && comp == 0){
+		if (strcmp(node->node_str, str) == 0){
+			comp = 1;
 		}
-      
-      if(comp != 0) {
-         if (!next) {
-            prev->next = node;
-         }
-         else {
-            if (prev) {
-               node->next = prev->next;
-               prev->next = node;
-            }
-            else {
-               node->next = list;
-               list = node;
-            }
-         }
-      }
 	}
-	return list;
+	if (comp == 1){
+		return NULL;
+	}
+	else 
+		return node;
+}
+
+void add_node(struct link_node	*list, char ID[]){
+	node *newNode = (node*)malloc(sizeof(node));
+	list->next = newNode;
+	strcpy(newNode->node_str, ID);
+	newNode->next = NULL;
 }
 
 void free_list(node *head) {
@@ -69,12 +66,12 @@ void free_list(node *head) {
 	}
 }
 
-int compare_node(struct link_node *n1, struct link_node *n2) {
+inline int compare_node(struct link_node *n1, struct link_node *n2) {
 	return strcmp(n1->node_str, n2->node_str);
 }
 
 void display_list(struct link_node *head) {
-	while (head) {
+	while (head != NULL) {
 		printf("%s \n", head->node_str);
 		head = head->next;
 	}
