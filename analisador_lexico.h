ab1c5d616da4f6044b49fbcdd26f04e9db93ae61
Alexandre Libório <alexandre_liborio@outlook.com>
2022-11-09 19:08:14 -0400

    Adiciona header e remove lex

---
+++ analisador_lexico.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define nSymbols 26
+enum {LETRA=nSymbols, DIGITO, INVALIDO};
+
+enum tKeyword {INTEGER, BOOLEAN, TRUE, FALSE, READ, WRITE, VOID, WHILE, IF, RETURN, GOTO, TYPES, FUNCTIONS, LABELS, VARS, VAR, ELSE, nKeywords};
+enum tTerminal {ID=nKeywords, CONST, ADDOP, MULOP, NOT, SEMICOLON, ASSIGN, COLON, COMMA, LCB, RCB, LB, RB, LP, RP, RELOP, FIM_ARQ, nTerminal};
+enum tErros{ERRO_LEXICO=nTerminal, ERRO_SINTATICO};
+extern int lines;
+extern char lexema[100];
+extern char *terminalName[];
+extern FILE *fin;
+
+char nextSymbol (FILE* fin);
+char checaKeyword (char *s);
+int lex();
