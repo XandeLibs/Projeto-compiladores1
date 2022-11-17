d7f7f823a08a052c519390fb00be66336efd35f5
Alexandre Libório <alexandre_liborio@outlook.com>
2022-11-16 20:55:43 -0400

    Move definições para o header

--- tabeladesimbolos.c
+++ tabeladesimbolos.c
@@ -4,62 +4,6 @@
 #include <string.h>
 #include "tabeladesimbolos.h"
 
-struct _TypeDescr {
-  TypeConstr constr;
-  int size;
-};
-
-typedef struct _Constant {
-  int value;
-  TypeDescrPtr type;
-} Constant;
-
-typedef struct _Variable {
-  int displ;
-  TypeDescrPtr type;
-} Variable;
-
-typedef struct _Formal_Pam {
-  int displ;
-  TypeDescrPtr type;
-  Passage pass;
-} Formal_Pam;
-
-typedef struct _Function {
-  int displ;
-  TypeDescrPtr result;
-  SymbEntry *params;
-} Function;
-
-typedef struct _Label {
-  char *label;
-  bool defined;
-} Label;
-
-typedef struct _Type {
-  TypeDescrPtr type;
-} Type;
-
-struct _symbEntry {
-  SymbCateg categ;
-  char *ident;
-  int level;
-  struct _symbEntry * next;
-
-  union {
-    Constant c;
-    Variable v;
-    Formal_Pam fm;
-    Function f;
-    Label l;
-    Type t;
-  } descr;
-};
-
-struct tabela {
-  SymbEntry * head; 
-};
-
 SymbEntry * createSymbEntry(SymbCateg categ, char *ident, int level){
   SymbEntry * newSymbEntry = malloc(sizeof(SymbEntry));
   if (!newSymbEntry) {
@@ -93,46 +37,46 @@ void display(Tabela * tabela) {
   }
 }
 
-void addConstant(SymbCateg categ, char *ident, int level, Tabela * tabela, int value, TypeDescrPtr type){
+void addConstant(char *ident, int level, Tabela * tabela, int value, TypeDescrPtr type){
   SymbEntry* entry;
-  entry = add(categ, ident, level, tabela);
+  entry = add(S_CONST, ident, level, tabela);
   entry->descr.c.value = value;
   entry->descr.c.type = type;
 }
 
-void addVariable(SymbCateg categ, char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr type){
+void addVariable(char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr type){
   SymbEntry* entry;
-  entry = add(categ, ident, level, tabela);
+  entry = add(S_VARIABLE, ident, level, tabela);
   entry->descr.v.displ = displ;
   entry->descr.v.type = type;
 }
 
-void addFormal_Param(SymbCateg categ, char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr type, Passage pass){
+void addFormal_Param(char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr type, Passage pass){
   SymbEntry* entry;
-  entry = add(categ, ident, level, tabela);
+  entry = add(S_PARAMETER, ident, level, tabela);
   entry->descr.fm.displ = displ;
   entry->descr.fm.type = type;
   entry->descr.fm.pass = pass;
 }
 
-void addFunction(SymbCateg categ, char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr result, SymbEntry *params){
+void addFunction(char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr result, SymbEntry *params){
   SymbEntry* entry;
-  entry = add(categ, ident, level, tabela);
+  entry = add(S_FUNCTION, ident, level, tabela);
   entry->descr.f.displ = displ;
   entry->descr.f.result = result;
   entry->descr.f.params = params;
 }
 
-void addLabel(SymbCateg categ, char *ident, int level, Tabela * tabela, char *label, bool defined){
+void addLabel(char *ident, int level, Tabela * tabela, char *label, bool defined){
   SymbEntry* entry;
-  entry = add(categ, ident, level, tabela);
+  entry = add(S_LABEL, ident, level, tabela);
   entry->descr.l.label = label;
   entry->descr.l.defined = defined;
 }
 
-TypeDescrPtr addType(SymbCateg categ, char *ident, int level, Tabela * tabela, TypeConstr predef_function, int size){
+TypeDescrPtr addType(char *ident, int level, Tabela * tabela, TypeConstr predef_function, int size){
   SymbEntry* entry;
-  entry = add(categ, ident, level, tabela);
+  entry = add(S_TYPE, ident, level, tabela);
   TypeDescr *description = malloc(sizeof(TypeDescr));
   description->constr = predef_function;
   description->size = size;
--- tabeladesimbolos.h
+++ tabeladesimbolos.h
@@ -2,22 +2,74 @@
 #define TABELA_HEADER
 #include <stdbool.h>
 
-typedef struct _symbEntry SymbEntry;
-typedef struct tabela Tabela;
-typedef struct _TypeDescr *TypeDescrPtr;
-typedef struct _TypeDescr TypeDescr;
+typedef struct _Constant {
+  int value;
+  TypeDescrPtr type;
+} Constant;
+
+typedef struct _Variable {
+  int displ;
+  TypeDescrPtr type;
+} Variable;
+
+typedef struct _Formal_Pam {
+  int displ;
+  TypeDescrPtr type;
+  Passage pass;
+} Formal_Pam;
+
+typedef struct _Function {
+  int displ;
+  TypeDescrPtr result;
+  SymbEntry *params;
+} Function;
+
+typedef struct _Label {
+  char *label;
+  bool defined;
+} Label;
+
+typedef struct _Type {
+  TypeDescrPtr type;
+} Type;
+
+typedef struct _symbEntry {
+  SymbCateg categ;
+  char *ident;
+  int level;
+  struct _symbEntry * next;
+
+  union {
+    Constant c;
+    Variable v;
+    Formal_Pam fm;
+    Function f;
+    Label l;
+    Type t;
+  } descr;
+}SymbEntry;
+
+typedef struct tabela {
+  SymbEntry * head; 
+}Tabela;
+
+typedef struct _TypeDescr {
+  TypeConstr constr;
+  int size;
+}*TypeDescrPtr, TypeDescr;
+
 typedef enum {S_CONST = 1, S_VARIABLE, S_PARAMETER, S_FUNCTION, S_LABEL, S_TYPE} SymbCateg;
 typedef enum {P_VALUE = 1, P_VARIABLE} Passage;
 typedef enum {T_PREDEF = 1, T_FUNCTION } TypeConstr;
 
 Tabela * maketabela();
 SymbEntry * createSymbEntry(SymbCateg categ, char *ident, int level);
-void addConstant(SymbCateg categ, char *ident, int level, Tabela * tabela, int value, TypeDescrPtr type);
-void addVariable(SymbCateg categ, char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr type);
-void addFormal_Param(SymbCateg categ, char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr type, Passage pass);
-void addFunction(SymbCateg categ, char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr result, SymbEntry *params);
-void addLabel(SymbCateg categ, char *ident, int level, Tabela * tabela, char *label, bool defined);
-TypeDescrPtr addType(SymbCateg categ, char *ident, int level, Tabela * tabela, TypeConstr predef_function, int size);
+void addConstant(char *ident, int level, Tabela * tabela, int value, TypeDescrPtr type);
+void addVariable(char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr type);
+void addFormal_Param(char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr type, Passage pass);
+void addFunction(char *ident, int level, Tabela * tabela, int displ, TypeDescrPtr result, SymbEntry *params);
+void addLabel(char *ident, int level, Tabela * tabela, char *label, bool defined);
+TypeDescrPtr addType(char *ident, int level, Tabela * tabela, TypeConstr predef_function, int size);
 SymbEntry* add(SymbCateg categ, char *ident, int data, Tabela * tabela);
 void display(Tabela * Tabela);
 SymbEntry* search(SymbCateg categ, char *ident, Tabela * tabela);
